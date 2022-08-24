047fa11aa43decae9a750fab25468292e8426806
projectperfect <jprfct@protonmail.com>
2022-08-24 00:14:05 -0500

    fetching data from api

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ parameters = {
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+question_data = data["results"]
 
 # question_data = [
 #     {
